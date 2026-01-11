e8f353ef6630f7fcff69bcd2092db7f2275fe89b
ezerfrlux <aaron.salinas318@gmail.com>
2026-01-11 16:44:53 -0600

    [UPDATE] - GetCarById implemented

--- src/cars/cars.controller.ts
+++ src/cars/cars.controller.ts
@@ -1,9 +1,21 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
 
 @Controller('cars')
 export class CarsController {
+  private cars = ['Toyota', 'Honda', 'Suzuki'];
+
   @Get()
   getAllCars() {
-    return ['Toyota', 'Honda', 'Suzuki'];
+    return this.cars;
+  }
+
+  @Get(':id')
+  getCarById(@Param('id') id: string) {
+    console.log({ id });
+    const car = this.cars.find((car, i) => i === +id)
+    return {
+      id,
+      car
+    };
   }
 }
